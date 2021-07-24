fb757e9f8ee3521cb868b91aca74cec4678e2ea9
Dinuwan Kalubowila <kalubowila.dc25132@live.com>
2021-07-24 19:18:57 +0530

    code seperated

---
+++ TasksApp.Services/TodoServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TasksApp.Models;
+
+namespace TasksApp.Services
+{
+    public class TodoServices : ITodoRepository
+    {
+        public List<Todo> AllTodos()
+        {
+
+            var todos = new List<Todo>();
+
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "Get Books",
+                Description = "Get books for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "Get Medicine",
+                Description = "Get Medicine for sickness",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.InProgress
+            };
+            todos.Add(todo2);
+
+            return todos;
+
+        }
+
+     
+    }
+}
--- TasksApp/Controllers/TodosController.cs
+++ TasksApp/Controllers/TodosController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TasksApp.Models;
+using TasksApp.Services;
 
 namespace TasksApp.Controllers
 {
@@ -12,47 +12,24 @@ namespace TasksApp.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
+        private readonly ITodoRepository _todoService;
+
+        public TodosController(ITodoRepository repository)
+        {
+            _todoService = repository;
+        }
+
         [HttpGet("{id?}")]
         public IActionResult GetTodos(int ? id)
         {
 
-            var myTodos = AllTodos();
+            var myTodos = _todoService.AllTodos();
             if (id == null) return Ok(myTodos);
 
-            myTodos = AllTodos().Where(t => t.Id == id).ToList();
+            myTodos = _todoService.AllTodos().Where(t => t.Id == id).ToList();
             return Ok(myTodos);
         }
 
-        //get Todos
-        private List<Todo> AllTodos()
-        {
-
-            var todos = new List<Todo>();
-
-            var todo1 = new Todo
-            {
-                Id = 1,
-                Title = "Get Books",
-                Description = "Get books for school",
-                Created = DateTime.Now,
-                Due = DateTime.Now.AddDays(5),
-                Status = TodoStatus.New
-            };
-            todos.Add(todo1);
-
-            var todo2 = new Todo
-            {
-                Id = 2,
-                Title = "Get Medicine",
-                Description = "Get Medicine for sickness",
-                Created = DateTime.Now,
-                Due = DateTime.Now.AddDays(2),
-                Status = TodoStatus.InProgress
-            };
-            todos.Add(todo2);
-
-            return todos;
-
-        }
+    
     }
 }
